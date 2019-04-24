ae521e2c5a838c6b7cc3f66fe6599a03a698a024
Divyanshu Goyal <divyanshugoyal@Divyanshus-MacBook-Air.local>
2019-04-25 00:24:42 +0530

    Initial Commit

---
+++ src/com/dibu/practice/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.dibu.practice;
+
+/**
+ * Created by divyanshugoyal on 4/25/19.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
